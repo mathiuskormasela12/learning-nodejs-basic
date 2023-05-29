ec526d94e22b719417aafc5c5db65e9dc86c3528
Mathius <mathiuskormasela12@gmail.com>
2023-05-29 22:46:32 +0700

    REPL

---
+++ 2. REPL/script.sh
@@ -0,0 +1,10 @@
+Node Js REPL
+
+Node Js REPL atau Node Js Read Evaluate Print Loop
+merupakan sebuah fitur yang ada pada node js untuk
+menulis kode-kode javascript tanpa harus untuk membuat
+file javascriptnya.
+
+Cara menjalankan REPL :
+cukup menjalankan perintah "node" 
+di terminal sistem operasi kita. 
